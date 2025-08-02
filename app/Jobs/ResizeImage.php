5280a5fb0d9fdb14d3cce073c7f674c442eb0fdf
Simone <scognac23@gmail.com>
2025-08-02 03:30:23 +0200

    user story 8-9 fine

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Spatie\Image\Image;
+use Spatie\Image\Unit;
 
 use Spatie\Image\CropPosition;
 
@@ -27,15 +28,23 @@ public function __construct($filePath, $w, $h)
     /**
      * Execute the job.
      */
-  public function handle(): void
+public function handle(): void
 {
     $w = $this->w;
     $h = $this->h;
     $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
-    $destPath = storage_path() . '/app/public/' . $this->path . '/crop_{$w}x{$h}_' . $this->fileName;
+    $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
 
     Image::load($srcPath)
         ->crop($w, $h, CropPosition::Center)
+        ->watermark(
+            base_path('resources/img/watermark.png'),
+            width: 50,
+            height: 50,
+            paddingX: 5,
+            paddingY: 5,
+            paddingUnit: Unit::Percent
+        )
         ->save($destPath);
 }
 }
--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -11,6 +11,7 @@ use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\File;
 use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\GoogleVisionLabelImage;
+use App\Jobs\RemoveFaces;
 
 use App\Jobs\ResizeImage;
 
@@ -51,16 +52,25 @@ public function store()
     ]);
   
 
+
 if (count($this->images) > 0) {
    foreach ($this->images as $image) {
        $newFileName = "articles/{$this->article->id}";
        $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
-       dispatch(new ResizeImage($newImage->path, 300, 300));
-       dispatch(new GoogleVisionSafeSearch($newImage->id));
-       dispatch(new GoogleVisionLabelImage($newImage->id));
+       //vecchio codice:
+       // dispatch(new ResizeImage($newImage->path, 300, 300));
+       // dispatch(new GoogleVisionSafeSearch($newImage->id));
+       // dispatch(new GoogleVisionLabelImage($newImage->id));
+       //nuovo codice:
+       RemoveFaces::withChain([
+           new ResizeImage($newImage->path, 300, 300),
+           new GoogleVisionSafeSearch($newImage->id),
+           new GoogleVisionLabelImage($newImage->id)
+       ])->dispatch($newImage->id);
    }
    File::deleteDirectory(storage_path('/app/livewire-tmp'));
 }
+
 
 
     
