e437c5538e2d51b5a92b16da8fbcd2c1bc038edb
Simone <scognac23@gmail.com>
2025-08-02 02:20:05 +0200

    User Story 6 completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -8,6 +8,10 @@ use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\File;
+
+use App\Jobs\ResizeImage;
+
 
 class CreateArticleForm extends Component
 {
@@ -43,13 +47,15 @@ public function store()
         'category_id' => $this->category,
         'user_id' => Auth::id()
     ]);
-
     if (count($this->images) > 0) {
         foreach ($this->images as $image) {
-            $this->article->images()->create(['path' => $image->store('images', 'public')]);
+            $newFileName = "articles/{$this->article->id}";
+            $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
+            dispatch(new ResizeImage($newImage->path, 300, 300));
         }
     }
-
+    File::deleteDirectory(storage_path('/app/livewire-tmp'));
+    
     session()->flash('success', 'Articolo creato correttamente');
     $this->cleanForm();
 }
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,6 +6,7 @@ use App\Models\Article;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;  
+use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
 {
@@ -18,5 +19,22 @@ class Image extends Model
     public function article(): BelongsTo
     {
         return $this->belongsTo(Article::class);
+    }
+
+    public static function getUrlByFilePath($filePath, $w = null, $h = null)
+    {
+        return Storage::url($filePath);
+        if (!$w && !$h) {
+            return Storage::url($filePath);
+        }
+        $path = dirname($filePath);
+        $filename = basename($filePath);
+        $file = "{$path}/crop_{$w}x{$h}_{$filename}";
+        return Storage::url($file);
+    }
+
+    public function getUrl($w = null, $h = null)
+    {
+        return self::getUrlByFilePath($this->path, $w, $h);
     }
 }
